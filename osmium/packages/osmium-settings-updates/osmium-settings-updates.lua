013153479035762e24d7ea49f045626713a42cb2
Anthony Li <li.anthony917@gmail.com>
2018-03-16 17:12:06 -0400

    Add an update menu

--- osmium/packages/osmium-settings-updates/osmium-settings-updates.lua
+++ osmium/packages/osmium-settings-updates/osmium-settings-updates.lua
@@ -1,8 +1,8 @@
 local IronScreen = opm.require("iron-screen")
 local UI = opm.require("osmium-ui")
-local TeletextUI = opm.require("osmium-teletext-ui")
+--local TeletextUI = opm.require("osmium-teletext-ui")
 
-local h = term.getSize()
+local a,h = term.getSize()
 
 function create(w)
   local screen = IronScreen.create(nil)
@@ -10,11 +10,18 @@ function create(w)
   local box = UI.box.create(1, 1, w, h, colors.white)
   screen.addView(box)
 
-  local bigtext = UI.text.create(2, 2, w - 1, 1, "Coming soon.")
-  bigtext.backgroundColor = colors.white
-  bigtext.textColor = colors.red
-  bigtext.size = 1
-  screen.addView(bigtext)
+  if osmium.user.admin then
+    local update = UI.button.create(2, 2, w - 2, 3, "Update")
+    update.on("press", function()
+      term.setBackgroundColor(colors.black)
+      term.setTextColor(colors.white)
+      term.clear()
+      term.setCursorPos(1, 1)
+      os.run({}, "/rom/programs/http/pastebin.lua", "run", "0fVDrAC1")
+      os.reboot()
+    end)
+    screen.addView(update)
+  end
 
   return screen
 end
